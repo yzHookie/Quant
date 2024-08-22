48ce0edc387c4cbf20ad75e8d8c0bec5f4578fd0
ZiyiHan <hanziyi@stu.scu.edu.cn>
2024-08-22 11:11:47 +0800

    Update binance_ccxt_api.py

--- test/binance_ccxt_api.py
+++ test/binance_ccxt_api.py
@@ -2,8 +2,6 @@ import ccxt
 
 exchange = ccxt.binance(
     {
-        "apiKey": "OGx3Q7YnvI6GZyGA8I9FS4MYspYep00g72yjvqyg6Ze6YtyL2rmU8GV6ke2YeK95",
-        "secret": "hy1eqcfiECpyDCbuNN5HZLkOnzE2ljnyK1SOedB8256F7M5hB7GHTdUotY7XnScN",
         "proxies": {
             "http": "http://127.0.0.1:7890",
             "https": "http://127.0.0.1:7890",
